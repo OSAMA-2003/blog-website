e0adac80694b776c3ddbd58cd8577415f25a9ab2
Osama <osama22@gmail.com>
2025-11-13 14:06:44 +0200

    test222

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from "@/components/ui/toaster"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Social App",
+  title: "Blog Plus",
   description: "Share and discover startup ideas.",
 }
 
